053eb6e434691e70dff33464af02845ce914564f
Bob McAleavey <bobmcaleavey@gmail.com>
2021-06-02 10:23:20 -0400

    multiple additions

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,3 +2,9 @@
   // css
   import '../css/style.css';
 </script>
+
+<svelte:head>
+  <title></title>
+</svelte:head>
+
+<div class="fixed top-0 left-0 w-screen h-screen -z-1" style="background-image:url(./design/mobile-design.jpg)"></div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,12 @@
 module.exports = {
-	mode: "jit",
-	purge: [
-		"./src/**/*.{html,js,svelte,ts}",
-	],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  mode: 'jit',
+  purge: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      zIndex: {
+        '-1': '-1',
+      },
+    },
+  },
+  plugins: [],
 };
