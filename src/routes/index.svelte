7d133ed33cb4f17f99aa71e358ae3d732beef518
Bob McAleavey <bobmcaleavey@gmail.com>
2021-06-03 07:37:58 -0400

    add main and container elements

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,3 +8,8 @@
 </svelte:head>
 
 <div class="fixed top-0 left-0 w-screen h-screen -z-1" style="background-image:url(./design/mobile-design.jpg)"></div>
+
+<main class="flex items-center justify-center min-h-screen">
+  <!-- container -->
+  <div class="flex flex-col items-center px-[2rem] py-[2rem]"></div>
+</main>
