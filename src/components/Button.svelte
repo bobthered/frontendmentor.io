4dfe9e8aabd44da387233eec6eb510fc99bf8dc9
Bob McAleavey <bobmcaleavey@gmail.com>
2021-06-07 08:22:10 -0400

    add text-center class

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,3 +1,3 @@
 <script>
 </script>
-<a href="#" class="rounded-full px-[2rem] py-[1rem] {$$props.class}"><slot/></a>
+<a href="#" class="rounded-full px-[2rem] py-[1rem] text-center {$$props.class}"><slot/></a>
