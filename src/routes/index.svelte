feab5c647e4d4c7ec38b2caea6ad87c252f2b506
Bob McAleavey <bobmcaleavey@gmail.com>
2021-06-07 13:05:47 -0400

    refactor design images

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,8 +7,8 @@
   <title></title>
 </svelte:head>
 
-<img src="./design/mobile-design.jpg" class="fixed top-0 left-0 w-[375px] h-[800px] opacity-20 -z-1 lg:hidden" />
-<img src="./design/desktop-design.jpg" class="fixed top-0 left-0 w-[1440] h-[800px] opacity-20 -z-1 hidden lg:block" />
 
-<main class="flex flex-col items-center justify-center min-h-screen">
+<main class="relative flex flex-col items-center justify-center min-h-screen">
+  <img src="./design/mobile-design.jpg" class="absolute top-0 left-0 w-[375px] h-[800px] opacity-20 -z-1 lg:hidden" />
+  <img src="./design/desktop-design.jpg" class="absolute top-0 left-0 w-[1440] h-[800px] opacity-20 -z-1 hidden lg:block" />
 </main>
