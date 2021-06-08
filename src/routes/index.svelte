7781635856632f25b01926b82d892cb7577daee1
Bob McAleavey <bobmcaleavey@gmail.com>
2021-06-08 12:07:40 -0400

    fix design image css class

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,5 +10,5 @@
 
 <main class="relative flex flex-col items-center justify-center min-h-screen">
   <img src="./design/mobile-design.jpg" class="absolute top-0 left-0 w-[375px] h-[800px] opacity-20 -z-1 lg:hidden" />
-  <img src="./design/desktop-design.jpg" class="absolute top-0 left-0 w-[1440] h-[800px] opacity-20 -z-1 hidden lg:block" />
+  <img src="./design/desktop-design.jpg" class="absolute top-0 left-0 w-[1440px] h-[800px] opacity-20 -z-1 hidden lg:block" />
 </main>
