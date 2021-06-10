3ca1f761930c453f6edb7bb6bc01c61f16386a65
Bob McAleavey <bobmcaleavey@gmail.com>
2021-06-10 13:39:36 -0400

    add 'href' prop

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,3 +1,6 @@
 <script>
+  // props
+  export let href = "#";
 </script>
-<a href="#" on:click class="rounded-full px-[2rem] py-[1rem] text-center {$$props.class}"><slot/></a>
+
+<a {href} on:click class="rounded-full px-[2rem] py-[1rem] text-center {$$props.class}"><slot/></a>
