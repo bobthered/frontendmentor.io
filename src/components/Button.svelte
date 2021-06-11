6ae60b9831b43950026c856dce679e45f52e98a4
Bob McAleavey <bobmcaleavey@gmail.com>
2021-06-11 08:26:01 -0400

    add ring classes for debugging

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -3,4 +3,4 @@
   export let href = "#";
 </script>
 
-<a {href} on:click class="rounded-full px-[2rem] py-[1rem] text-center {$$props.class}"><slot/></a>
+<a {href} on:click class="ring ring-1 ring-red-500 rounded-full px-[2rem] py-[1rem] text-center {$$props.class}"><slot/></a>
