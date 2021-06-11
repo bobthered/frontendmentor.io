e17d1df0d858f8151c5a50dcdb2920055d5f86ec
Bob McAleavey <bobmcaleavey@gmail.com>
2021-06-11 08:27:58 -0400

    add multiple props and classes

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,7 +1,14 @@
 <script>
-  // props
+  // props ( external )
+  export let padding = 'p-[1rem]'
+  export let rounded = 'rounded-xl';
+  export let shadow = '';
+  export let theme = 'default';
+
+  // props ( dynamic )
+  $: themeClasses = theme === 'default' ? '' : ''
 </script>
 
-<div class="rounded-xl flex flex-col bg-opacity-0 {$$props.class}">
+<div class="ring ring-1 ring-red-500 flex flex-col bg-opacity-0 {padding} {rounded} {shadow} {themeClasses} {$$props.class}">
   <slot/>
 </div>
