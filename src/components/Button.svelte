cae6e004850aed01e47d2671bd6413cb972dd2ab
Bob McAleavey <bobmcaleavey@gmail.com>
2021-06-11 08:31:56 -0400

    add multiple props and classes

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,6 +1,13 @@
 <script>
-  // props
-  export let href = "#";
+  // props ( external )
+  export let href = '#';
+  export let padding = 'px-[2rem] py-[1rem]'
+  export let rounded = 'rounded-full';
+  export let shadow = '';
+  export let theme = 'default';
+
+  // props ( dynamic )
+  $: themeClasses = theme === 'default' ? '' : '';
 </script>
 
-<a {href} on:click class="ring ring-1 ring-red-500 rounded-full px-[2rem] py-[1rem] text-center {$$props.class}"><slot/></a>
+<a {href} on:click class="ring ring-1 ring-red-500 text-center {padding} {rounded} {shadow} {themeClasses} {$$props.class}"><slot/></a>
