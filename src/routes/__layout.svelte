c32119ff9c4a59e000ed548419cf12b9ac7a6a5c
Bob McAleavey <bobmcaleavey@gmail.com>
2021-06-15 12:04:26 -0400

    add Header component and remove design files

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,10 +1,12 @@
 <script>
+  // components
+  import Header from '../components/Header.svelte';
+
   // css
   import '../css/style.css';
 </script>
 
 <main class="relative flex flex-col">
+  <Header />
   <slot/>
-  <img src="./design/mobile-design.jpg" class="absolute top-0 left-0 w-[375px] h-[800px] opacity-20 -z-1 lg:hidden" />
-  <img src="./design/desktop-design.jpg" class="absolute top-0 left-0 w-[1440px] h-[800px] opacity-20 -z-1 hidden lg:block" />
 </main>
